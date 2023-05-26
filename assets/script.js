8280fe6890dce3d758f8aa130781bfee23eb8002
Daniel Morriss <danielmorriss1@gmail.com>
2023-05-26 11:16:53 +0100

    add js functionality to open and close the rules container

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
-console.log('the js file is linked');
+//button variables
+const playButton = document.getElementById('play-button');
+const rulesButton = document.getElementById('rules-button');
+const backButton = document.getElementById('back-button');
+
+//containers
+const homeContainer = document.getElementById('home-container');
+const rulesContainer = document.getElementById('rules-container');
+
+//Open the rules container from the home screen
+rulesButton.addEventListener('click', () => {
+    homeContainer.classList.add('hide');
+    rulesContainer.classList.remove('hide');
+})
+
+//Close the rules container
+backButton.addEventListener('click', () => {
+    rulesContainer.classList.add('hide');
+    homeContainer.classList.remove('hide');
+})
